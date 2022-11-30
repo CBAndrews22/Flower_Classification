32a2c3154b4d75948aa210728f9b1ea9ad71efc3
Chad Andrews <chad_a@Chads-MacBook-Pro.local>
2022-11-30 08:43:45 -0800

    Adding small data set for testing

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+int main(){
+    // Prompt user for file name
+
+    // Prompt user for forward search or back elimination
+
+    
+
+
+}
